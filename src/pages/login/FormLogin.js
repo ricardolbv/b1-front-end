280914aec522cf5de4fb84f7958cf1abe403d3ba
Ricardo Vieira <ricardo.biscola.vieira@gmail.com>
2021-04-12 08:31:09 -0300

    Inserindo campos do form

--- src/pages/login/FormLogin.js
+++ src/pages/login/FormLogin.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const useStyle = makeStyles({
     Form: {
@@ -25,6 +27,21 @@ const useStyle = makeStyles({
         font: "Roboto",
         marginTop: "45px",
     },
+    Input :{
+        width: '300px',
+        height: '40px',
+        marginTop: '35px',
+    },
+    Forgot :{
+        marginTop: '20px',
+        marginRight: "40px"
+    },
+    Btn: {
+        backgroundColor: "#0D0B23",
+        width: '300px',
+        height: '40px',
+        marginTop: '35px',
+    }
 })
 
 function FormLogin() {
@@ -34,6 +51,22 @@ function FormLogin() {
             <form>
                 <Typography className={classes.PrimaryColor} variant='h5' align="center">Bem vindo!</Typography>
                 <Typography variant='subtitle1' align="center" className={classes.GreyText}>Faça seu login</Typography>
+                <Typography align="center">
+                    <TextField id="email" label="E-mail" variant="outlined" className={classes.Input} />
+                </Typography>
+                <Typography align="center">
+                <TextField id="senha" label="Digite sua senha" variant="outlined" className={classes.Input} />
+                </Typography>
+                <a href="https://material-ui.com/api/text-field/">
+                <Typography align="right" className={classes.Forgot}>
+                    Esqueceu a senha?
+                </Typography>
+                </a>
+                <Typography align="center">
+                <Button variant="contained" color="primary" className={classes.Btn}>
+                    Entre
+                </Button>
+                </Typography>
             </form>
         </Paper>
     )
