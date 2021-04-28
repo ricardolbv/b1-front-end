fa0f1d5061c5bd8cf234e2a4ae15dca0bddc6192
Ricardo Vieira <ricardo.biscola.vieira@gmail.com>
2021-04-28 20:02:23 -0300

    Atualizando master/dev

---
+++ src/pages/retail/RetailPage.js
@@ -0,0 +1,15 @@
+import Typography from '@material-ui/core/Typography';
+import BuildIcon from '@material-ui/icons/Build';
+import Container from '@material-ui/core/Container';
+
+const RetailPage = () => {
+    return (
+        <Container>
+            <Typography variant='h2' textAlign='center'> Pagina Varejo </Typography>
+            <BuildIcon />
+            
+        </Container>
+    )
+}
+
+export default RetailPage;
