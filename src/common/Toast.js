cbc72a93dfa1c35dfd0f209213bab2f7a178fba4
Ricardo Vieira <ricardo.biscola.vieira@gmail.com>
2021-04-28 21:07:48 -0300

    Implementação de toast

---
+++ src/common/Toast.js
@@ -0,0 +1,27 @@
+import { Alert, AlertTitle } from '@material-ui/lab';
+import SnackBar from '@material-ui/core/Snackbar';
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+    root: {
+        width: '50%',
+        position: 'fixed',
+        top:'130px'
+    }
+})
+
+const Toast = props => {
+    const classes = useStyles();
+
+    return (
+        <div >
+        <SnackBar open={props.open} className={classes.root}  onClick={props.handleClose} autoHideDuration={6000}>
+        <Alert severity={props.status} variant='filled' className={classes.root} open={props.open} onClose={props.handleClose} >
+            <AlertTitle>{props.message}</AlertTitle>
+        </Alert>
+        </SnackBar>
+        </div>
+    )
+}
+
+export default Toast;
--- src/pages/home/HomeDisplay.js
+++ src/pages/home/HomeDisplay.js
@@ -1,12 +1,43 @@
+import { useState } from 'react';
 import Typography from '@material-ui/core/Typography';
 import BuildIcon from '@material-ui/icons/Build';
 import Container from '@material-ui/core/Container';
+import Toast from '../../common/Toast';
+import { Button } from '@material-ui/core';
 
 const HomeDisplay = () => {
+    //Message status
+    const [toast, setToast] = useState(false);
+    const [status, setStatus] = useState("success");
+    const [message, setMessage] = useState("");
+
+    const handleClose = () => setToast(false);
     return (
         <Container>
             <Typography variant='h2' textAlign='center'> Pagina Inicial </Typography>
             <BuildIcon />
+            <div>
+                <Button variant='contained' onClick={() => {
+                    setToast(true) 
+                    setMessage('Aviso: Mensagem de Aviso...') 
+                    setStatus("warning")         
+                }}> Toast de Aviso 
+                </Button>
+                <Button variant='contained' onClick={() => {
+                    setToast(true) 
+                    setMessage('Aviso: Mensagem de Erro...') 
+                    setStatus("error")         
+                }}> Toast de Erro 
+                </Button>
+                <Button variant='contained' onClick={() => {
+                    setToast(true) 
+                    setMessage('Aviso: Mensagem de Sucesso...') 
+                    setStatus("success")         
+                }}> Toast de Sucesso 
+                </Button>
+               
+            </div>
+            <Toast status={status} message={message} open={toast} handleClose={handleClose}/>
        </Container>
     )
 }
