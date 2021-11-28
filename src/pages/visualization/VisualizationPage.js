0a4b9b1156392d90c74160f0105cc833bf3aee4b
Ricardo Vieira <ricardo.biscola.vieira@gmail.com>
2021-11-28 19:44:39 -0300

    Atualizando o Power BI

--- src/pages/visualization/VisualizationPage.js
+++ src/pages/visualization/VisualizationPage.js
@@ -6,7 +6,7 @@ import Paper from '@material-ui/core/Paper';
 const VisualizationPage = () => {
     return (
         <Box display='flex' justifyContent='center' m={1} p={2} style={{ width: '90%', height: '70%'}}>
-        <div dangerouslySetInnerHTML={{ __html: "<iframe width='1000' height='800' src='https://app.powerbi.com/reportEmbed?reportId=4c24ff1f-a720-4050-8bf4-2f024d5790e4&autoAuth=true&ctid=610e9057-3c4f-4b45-b8a0-095ac9ef7121&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWJyYXppbC1zb3V0aC1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D frameborder='0' allowFullScreen='true'></iframe>"}} />
+        <div dangerouslySetInnerHTML={{ __html: "<iframe width='1000' height='800' src='https://app.powerbi.com/reportEmbed?reportId=cc525e6b-b425-4e6e-899e-d1bbedcc9867&autoAuth=true&ctid=080bc023-5e5a-46fb-bbe8-eb548e9873a6&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWJyYXppbC1zb3V0aC1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D frameborder='0' allowFullScreen='true'></iframe>"}} />
        </Box>
     )
 }
